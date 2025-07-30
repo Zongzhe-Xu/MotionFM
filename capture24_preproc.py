60052269df68b8941a80d6173e9edf7ce925dc5b
sarah jiang <sarahjiang@sarahs-MacBook-Air-5.local>
2025-07-30 16:53:28 -0400

    take 2

--- capture24_preproc.py
+++ capture24_preproc.py
@@ -35,7 +35,10 @@ def preprocess_file(input_file: Path, output_dir: Path, downsampling_ratio: int)
 
     # Reorder columns
     cols = ['time', 'x', 'y', 'z', 'activity', 'MET']
-    df = df[cols]
+    if 'annotation' in df.columns:
+        df = df[cols]
+    else:
+        df = df[cols]
 
     # Save to new path
     output_file = output_dir / input_file.name
