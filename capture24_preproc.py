0eeeb93e4e93ead7138e90642e5ed5572c9afc2d
sarah jiang <sarahjiang@wireless152023028182.med.unc.edu>
2025-08-18 15:19:45 -0400

    butterworth

--- capture24_preproc.py
+++ capture24_preproc.py
@@ -157,7 +157,7 @@ def preprocess_file(
         chunk_start_str = chunk_start_time.strftime("%Y%m%dT%H%M%S")
 
         # Choose columns to save
-        save_cols = (["time"] if keep_time else []) + ["x", "y", "z", "ENMO", "activity", "MET"]
+        save_cols = (["time"] if keep_time else []) + ["x", "y", "z", "ENMO", "activity", "MET"] ## drop enmo, activity, MET, generate separate labels file for this chunk with activity present
         save_df = chunk_df.drop(columns=["annotation"], errors="ignore")[save_cols]
 
         # Write parquet
